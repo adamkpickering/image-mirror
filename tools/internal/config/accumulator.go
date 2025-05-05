b9811c5be40621ccf35ebacd1ee4304f72f514ae
Adam Pickering <adam.pickering@suse.com>
2025-05-05 16:58:38 -0600

    Allow passing multiple images in accumulator.AddImages

--- tools/internal/config/accumulator.go
+++ tools/internal/config/accumulator.go
@@ -19,21 +19,23 @@ func NewImageAccumulator() *ImageAccumulator {
 	}
 }
 
-func (ia *ImageAccumulator) AddImage(newImage *Image) {
-	pair := sourceTargetPair{
-		SourceImage:     newImage.SourceImage,
-		TargetImageName: newImage.TargetImageName(),
-	}
-	existingImage, ok := ia.mapping[pair]
-	if !ok {
-		ia.mapping[pair] = newImage
-	} else {
-		for _, newTag := range newImage.Tags {
-			if !slices.Contains(existingImage.Tags, newTag) {
-				existingImage.Tags = append(existingImage.Tags, newTag)
-			}
+func (ia *ImageAccumulator) AddImages(newImages ...*Image) {
+	for _, newImage := range newImages {
+		pair := sourceTargetPair{
+			SourceImage:     newImage.SourceImage,
+			TargetImageName: newImage.TargetImageName(),
 		}
-		ia.mapping[pair] = existingImage
+		existingImage, ok := ia.mapping[pair]
+		if !ok {
+			ia.mapping[pair] = newImage
+		} else {
+			for _, newTag := range newImage.Tags {
+				if !slices.Contains(existingImage.Tags, newTag) {
+					existingImage.Tags = append(existingImage.Tags, newTag)
+				}
+			}
+			ia.mapping[pair] = existingImage
+		}
 	}
 }
 
